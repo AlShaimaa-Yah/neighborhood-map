630f9d9795588be7b4bb0738a69bcd93ab5db380
AlShaimaa-Yah <35180096+AlShaimaa-Yah@users.noreply.github.com>
2018-02-04 22:45:20 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# neighborhood-map
-neighborhood-map
+#Neighborhood Map Project
+In this project, the task is to develop a single page application featuring a map of a neighborhood.
+This app is to demonstrate the ability to use a js framework (`knockout.js`) & Google Maps API.
+
+##Features
+The site is fully responsive and will run on your tiniest screens all the way to the largest screen you can think of.
+
+##API
+Google Maps API is used here to show the map and generate the markers etc.
+
+## Installation
+Clone or download this repo and simply open the index.html file and enjoy!
+You can make modifications to the locations and add your own in the `js/markers.js` file.
+
+##Here is The code
+You can find a live running site of this code [here]().
---
+++ js/markers.js
@@ -0,0 +1,50 @@
+var myLocations = [
+    {
+        title: 'Kabah',
+        lat: 21.422487,
+        lng:  39.826206,
+        type: 'AlHaram'
+    },
+    {
+        title: 'AlNor Hospital',
+        lat: 21.385031451289365,
+        lng: 39.860544204711914,
+        type: 'Hospital'
+    },
+    {
+        title: 'Ad Diyafah',
+        lat: 21.43584459802708,
+        lng: 39.8001709068194,
+        type:  'Mall'
+    },
+    {
+        title: 'Albaik',
+        lat: 21.421111484957944,
+        lng: 39.82166290283203,
+        type:  'Restaurant'
+    },
+    {
+        title: 'Ghar Thowr',
+        lat: 21.37723891309099,
+        lng: 39.84972953796387,
+        type: 'mountain'
+    },
+    {
+        title: 'Ghar Hira',
+        lat: 21.4575864,
+        lng: 39.859275600000046,
+        type: 'mountain'
+    },
+    {
+        title: 'Jarir',
+        lat: 21.44484033655246,
+        lng: 39.86045837402344,
+        type: 'Bookstore'
+    },
+    {
+        title: 'Abraj Albait',
+        lat: 21.4187514,
+        lng: 39.82555639999998,
+        type: 'Tower'
+    }
+]
